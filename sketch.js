38a7346062a350611ee9deb9d5a9a680d75d937f
yzx1229 <413730119@o365.tku.edu.tw>
2025-05-02 10:22:07 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -20,8 +20,12 @@ function draw() {
   let x = (width - capture.width) / 2;
   let y = (height - capture.height) / 2;
   
-  // 繪製攝影機影像
-  image(capture, x, y, capture.width, capture.height);
+  // 翻轉畫布以顯示鏡像
+  push();
+  translate(width, 0); // 將畫布原點移到右上角
+  scale(-1, 1); // 水平翻轉畫布
+  image(capture, x, y, capture.width, capture.height); // 繪製影像
+  pop();
 }
 
 function windowResized() {
